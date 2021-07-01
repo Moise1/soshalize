18ef23bb15ffaea15cbde49f23c7feb04a939854
Moise1 <rwibutso.mo@gmail.com>
2021-07-01 19:09:11 +0200

    finish CRUD ops on User model

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -39,12 +39,51 @@ class UserController {
         }
     }
 
-    static async EditUser(req, res){
-        const {_id} = req.params;
-         
+    static async FindUser(req, res){
+
+        const {id} = req.params;
+        try {
+            const user = await User.findById(id);
+            if (!user) return res.status(404).json('User not found');
+            return res.status(200).json({message: "Success", user});
+
+        } catch (error) {
+            if(error) return res.status(500).json(error.message)
+        }
     }
+
+    static async EditUser(req, res){
+        let {userId, password} = req.body;
+        const {id} = req.params;
+
+        try {
+            if(userId === id  || req.user.isAdmin){
+                if(password) password = await hashedPassword(password);
+                await User.findByIdAndUpdate(id, {$set: req.body, password});
+                return res.status(200).json('Account successfully updated.')
+            }else{
+                return res.status(403).json("You can update only your account.")
+            }
+
+        } catch (error) {
+            if(error) return res.status(500).json(error.message);
+        }
+    }
+
+    static async DeleteUser(req, res){
+         const {id} = req.params;
+        try {
+            const user = await User.findById(id);
+            if (!user) return res.status(404).json('User not found');
+            await User.deleteOne(user);
+            return res.status(200).json('User successfully deleted.');
+        } catch (error) {
+            if(error) return res.status(500).json(error.message)
+        }
+    }
+
+    
 }
 
 export default UserController;
 
-// Stopped at 41:49
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,8 @@ const router = express.Router();
 // User routes
 router.post('/api/v1/signup', UserController.Signup)
 router.post('/api/v1/login', UserController.Login);
-router.put('/api/v1/users/edit/:id', ()=> {});
+router.get('/api/v1/users/:id', UserController.FindUser);
+router.put('/api/v1/users/edit/:id', UserController.EditUser);
+router.delete('/api/v1/users/:id', UserController.DeleteUser);
 
 export default router;
